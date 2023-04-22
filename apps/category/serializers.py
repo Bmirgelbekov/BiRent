1a29c7d4de89030cbd6c4b53f15b1a20ffeb5309
enkasmaliev <erzhan112399@gmail.com>
2023-04-22 15:46:46 +0600

    urls

--- apps/category/serializers.py
+++ apps/category/serializers.py
@@ -7,8 +7,8 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['apartments'] = [
-    #         {'title': apartment.title, 'slug': apartment.slug} for apartment in instance.apartments.all()]
-    #     return representation
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['apartments'] = [
+            {'title': apartment.title, 'slug': apartment.slug} for apartment in instance.apartments.all()]
+        return representation

61e3106e52749b0e3d344cca9b859cd18d740a47
enkasmaliev <erzhan112399@gmail.com>
2023-04-24 20:06:49 +0600

    static

--- core/urls.py
+++ core/urls.py
@@ -45,3 +45,7 @@ urlpatterns = [
     path('', include('apps.apartment.urls')),
     path('', include('apps.category.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

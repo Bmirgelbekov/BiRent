431527e5ecd1668350dfb6a19a6426c6623880d6
enkasmaliev <erzhan112399@gmail.com>
2023-04-27 20:53:23 +0600

    reservation updated and fixed

--- apps/reservation/serializers.py
+++ apps/reservation/serializers.py
@@ -10,14 +10,27 @@ class ReservationItemSerializer(serializers.ModelSerializer):
 
 
 class ReservationSerializer(serializers.ModelSerializer):
-    reservation_items = ReservationItemSerializer(many=True, write_only=True)
+    reservated_room = ReservationItemSerializer(many=True, write_only=True)
 
     class Meta:
         model = Reservation 
         fields = '__all__'
         read_only_fields = ['total_cost', 'created_at', 'updated_at', 'user', 'status']
         
-    def create(self, validated_data):
-        query = super().create(validated_data)
-        query['reservation_item'] = ReservationItem
-        return query
+    def create(self, validated_data: dict):
+        validated_data['user'] = self.context.get('request').user
+        items = validated_data.pop('reservated_room')
+        reservation = Reservation.objects.create(**validated_data)
+        total_reservation_cost = 0
+        apartments = []
+        for item in items:
+            apartment = ReservationItem(
+                reservation=reservation,
+                apartment=item['apartment'],
+                duration=item['duration']
+            )
+            apartments.append(apartment)
+            total_reservation_cost += apartment.total_cost
+        reservation.total_cost = total_reservation_cost
+        ReservationItem.objects.bulk_Create(apartments)
+        return reservation
